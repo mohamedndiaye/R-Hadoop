1ef6fcd5d6500095f9937f0cbb6e7a6015765976
Youssef de Madeen Amadou <youssef.amadou@yahoo.fr>
2017-05-23 10:48:37 -0400

    Update reducer.r

--- Wordcount/reducer.r
+++ Wordcount/reducer.r
@@ -1,9 +1,9 @@
 #!/usr/bin/env Rscript
 #
-# Created by Youssef de Madeen Amadou, 23-May-2017
-# Reducer script for wordcount on HADOOP
+# Created by Youssef de Madeen Amadou on 23-May-2017
+# R script to run wordcount on Hadoop framework
 #
-f<-file("stdin",open = 'r')
+f <- file("stdin",open = 'r')
 ds = readLines(f,warn = F)
 sum = summary(factor(unlist(strsplit(ds,"\\s|\\s+"))))
 do.call(function(x) write(paste(names(x),":",x), stdout(),append = TRUE),list(sum))
