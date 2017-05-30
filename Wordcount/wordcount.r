26af2d5eb6e8e3414e3295c22c726c60f51d99a3
Youssef de Madeen Amadou <youssef.amadou@yahoo.fr>
2017-05-30 12:48:27 -0400

    Update wordcount.r

--- Wordcount/wordcount.r
+++ Wordcount/wordcount.r
@@ -1,11 +1,9 @@
 #!/usr/bin/env Rscript
 #
-# Created by Youssef de Madeen Amadou on 23-May-2017
-# R script to run wordcount on Hadoop framework
+# Created by Youssef de Madeen Amadou on 23-May-2017 R script to run wordcount on Hadoop framework
 #
 f <- file("stdin",open = 'r')
-ds = readLines(f,warn = F)
-sum = summary(factor(unlist(strsplit(ds,"\\s|\\s+"))))
-do.call(function(x) write(paste(names(x),":",x), stdout(),append = TRUE),list(sum))
+tmp1 = readLines(f,warn = F)
+write.table(table(factor(unlist(strsplit(tmp1,"\\s|\\s+")))),quote = FALSE,row.names = FALSE,col.names = FALSE,sep = "\t")
 close(f)
 #END
